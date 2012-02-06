c89506d5bcdb2b9fb0878a735d68d833019e95e3
ongaeshi <ongaeshi0621@gmail.com>
2012-02-07 02:12:18 +0900

    windowを一枚に

--- Resources/app.js
+++ Resources/app.js
@@ -8,69 +8,39 @@ var util = require('util');
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
-// create tab group
+// タブグループ
 var tabGroup = Titanium.UI.createTabGroup();
 
-
 //
-// create base UI tab and root window
+// メインウィンドウ
 //
-var win1 = Titanium.UI.createWindow({  
-    title:'TimeCopy',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
+var win = Titanium.UI.createWindow({  
+  title:'TimeCopy',
+  backgroundColor:'#fff'
 });
 
-win1.add(util.createButton('2012年2月7日(水) 00:07:00', 10));
-win1.add(util.createButton('2012年2月7日(水) 00:07'   , 70));
-win1.add(util.createButton('平成24年2月7日(水) 00:07' , 130));
-win1.add(util.createButton('2012/02/07 01:11'         , 190));
-win1.add(util.createButton('2月7日(水) 00:07'         , 250));
+win.add(util.createButton('2012年2月7日(水) 00:07:00', 10));
+win.add(util.createButton('2012年2月7日(水) 00:07'   , 70));
+win.add(util.createButton('平成24年2月7日(水) 00:07' , 130));
+win.add(util.createButton('2012/02/07 01:11'         , 190));
+win.add(util.createButton('2月7日(水) 00:07'         , 250));
 
-var label1 = Titanium.UI.createLabel({
+var label = Titanium.UI.createLabel({
   color:'#999',
-  text:'ボタンを押すと現在時刻を\nクリップボードにコピーします。',
+  text:'ボタンを押せば現在時刻をクリップボードにコピー',
   font:{fontSize:12},
   textAlign:'center',
   width:'auto',
   top: 310
 });
 
-win1.add(label1);
+win.add(label);
+win.hideTabBar();
 
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
+// タブ設定
+var tab = Titanium.UI.createTab({window: win});
+tabGroup.addTab(tab);
 
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
+// 表示
 tabGroup.open();
+
